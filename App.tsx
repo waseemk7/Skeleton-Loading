472dc0104cb4c81db8e62735707def6bf1b47290
Waseem Kurne <kurne.waseem@gmail.com>
2024-04-25 15:43:16 +0530

    contacts fetch from url

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { SafeAreaView, Text, View } from "react-native";
 
+type ContactInfo = {
+  id: number;
+  name: string;
+  email: string;
+};
+
 function App(): React.JSX.Element {
+  const [contacts, setConatcts] = useState<ContactInfo[] | undefined>();
+
+  const fetchContacts = useCallback(async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    setConatcts(data);
+  }, []);
+
+  useEffect(() => {
+    fetchContacts();
+  }, []);
+
   return (
     <SafeAreaView>
       <View>
